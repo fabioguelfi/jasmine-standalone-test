2b876e7669e58eb9bfb0d01263b73fe03a8a41b5
fabio guelfi <fabioguelfiunix@gmail.com>
2018-03-17 22:20:48 -0300

    and.callThrough

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -3,20 +3,20 @@ describe("testes do objeto spy", () => {
     let Calculadora = {
         somar: (n1, n2) => {
             return n1 + n2
+        },
+        subtrair: (n1, n2) => {
+            return n1 - n2
         }
     }
 
     beforeAll(() => {
-        spyOn(Calculadora, "somar")
+        spyOn(Calculadora, "somar").and.callThrough()
+        spyOn(Calculadora, "subtrair")
     })
 
-    it("deve chamar o metodo somar com os parametros validos", () => {
-
-        Calculadora.somar(1, 1)
-        Calculadora.somar(7, 7)
-        expect(Calculadora.somar).toHaveBeenCalledWith(1, 1)
-        expect(Calculadora.somar).toHaveBeenCalledWith(7, 7)
-
+    it("deve executar o metodo somar original", () => {
+        expect(Calculadora.somar(1, 1)).toEqual(2)
+        expect(Calculadora.subtrair(2, 1)).toBeUndefined
     })
 
 })
