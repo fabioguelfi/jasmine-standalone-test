8fb195081d9fa725a3aa43f4110026813083e41e
fabio guelfi <fabioguelfiunix@gmail.com>
2018-03-17 22:28:40 -0300

    and.returnValue

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -4,19 +4,21 @@ describe("testes do objeto spy", () => {
         somar: (n1, n2) => {
             return n1 + n2
         },
+
         subtrair: (n1, n2) => {
             return n1 - n2
         }
     }
 
     beforeAll(() => {
-        spyOn(Calculadora, "somar").and.callThrough()
+        spyOn(Calculadora, "somar").and.returnValue(10)
         spyOn(Calculadora, "subtrair")
     })
 
-    it("deve executar o metodo somar original", () => {
-        expect(Calculadora.somar(1, 1)).toEqual(2)
-        expect(Calculadora.subtrair(2, 1)).toBeUndefined
+    it("deve retornar 10 para o metodo somar", () => {
+        
+        expect(Calculadora.somar(7, 7)).toEqual(10)
+
     })
 
 })
