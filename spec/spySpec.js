3ffd2faf6fb9f3038eaec12c28bbb2ca8dc8ae18
fabio guelfi <fabioguelfiunix@gmail.com>
2018-03-29 20:01:43 -0300

    jasmine.createSpy

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -1,17 +1,21 @@
-describe("testes do objeto jasmine.objectContaing", () => {
-    let carro
+describe("testes do objeto jasmine.stringMAtching", () => {
+
+    let exibirTexto
 
     beforeAll(() => {
-        carro = {
-            carro = {
-                'ano': 2017
-            }
-        }
+        exibirTexto = jasmine.createSpy("exibirTexto")
     })
 
-    it("deve demonstrar o uso do jasmine.objectContaing", () => {
-        expect(carro).toEqual(jasmine.objectContaing({ 'ano': 2017 }));
-        expect(carro).not.toEqual(jasmine.objectContaing({ 'anos': 2017 }))
+    it("deve demonstrar o uso do jasmine.stringMatching", () => {
+        exibirTexto("curso de Jasmine")
+
+        expect(exibirTexto).toHaveBeenCalledWith(
+            jasmine.stringMatching("Jasmine"));
+        expect(exibirTexto).toHaveBeenCalledWith(
+            jasmine.stringMatching(/jasmine/i));
+        expect("CUrso de Javascript").toEqual(
+            jasmine.stringMatching("Javascript"));    
+
     })
 
 })
